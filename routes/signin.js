995d132fada93014565f5bed56ca8dbe4e1fe241
0uta <166687901+0uta@users.noreply.github.com>
2024-07-11 05:21:10 +0000

    第16章 終了

--- routes/signin.js
+++ routes/signin.js
@@ -4,14 +4,19 @@ const knex = require("../db/knex");
 
 
 router.get('/', function (req, res, next) {
+  const userId = req.session.userid;
+  const isAuth = Boolean(userId);
   res.render('signin', {
     title: 'Sign in',
+    isAuth: isAuth,
   });
 });
 
 router.post('/', function (req, res, next){
-    const username = req.body.username;
-    const password = req.body.password;
+  const userId = req.session.userid;
+  const isAuth = Boolean(userId); 
+  const username = req.body.username;
+  const password = req.body.password;
 
     knex("users")
     .where({
@@ -24,6 +29,7 @@ router.post('/', function (req, res, next){
         res.render("signin", {
           title: "Sign in",
           errorMessage: ["ユーザが見つかりません"],
+          isAuth: isAuth,
         });
       } else {
         req.session.userid = results[0].id;
@@ -35,7 +41,7 @@ router.post('/', function (req, res, next){
       res.render("signin", {
         title: "Sign in",
         errorMessage: [err.sqlMessage],
-        isAuth: false,
+        isAuth: isAuth,
       });
     });
 });
